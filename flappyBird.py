beed66909e59015b6157e547b006a9d1a4ee9907
Anikait <anikaitkapur@gmail.com>
2025-03-05 19:26:23 -0500

    implemented collision

--- flappyBird.py
+++ flappyBird.py
@@ -16,9 +16,6 @@ PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
-
-    
-
 class Bird:
     IMGS = BIRD_IMG
     MAX_ROTATION = 25
@@ -88,6 +85,56 @@ class Bird:
 
     def get_mask(self):
         return pygame.mask.from_surface(self.image)
+
+
+class Pipe:
+    GAP = 200
+    VELOCITY = 5
+
+    def __init__(self,x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom - 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True) # Flipped Pipe
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+
+    def set_heigt(self):
+        self.set_heigt = random.randrange
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+    
+    def move(self):
+        self.x -= self.VELOCITY
+    
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self. top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        bottom_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        top_point = bird_mask.overlap(top_mask, top_offset)
+    
+
+        if top_point or bottom_point:
+            return True
+        else:
+            return False
+
+
+
+
     
 def draw_window(win, bird):
     win.blit(BG_IMG, (0,0))
